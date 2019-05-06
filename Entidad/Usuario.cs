d58733d13becd349c6970c95e64000cccc20c77a
GedGonz <gedgonz7@gmail.com>
2019-05-05 19:47:08 -0600

    Agregando Entidad Usuario, Contexto y nuevo servicio en Startup

---
+++ Entidad/Usuario.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Entidad
+{
+    public class Usuario: IdentityUser
+    {
+        public Persona Persona { get; set; }
+    }
+}
--- MVC/Startup.cs
+++ MVC/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Contexto;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -30,6 +32,9 @@ namespace MVC
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
+            services.AddDbContext<Context>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
